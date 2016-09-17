55b481a38ed94fd4c5bb8e689d136d2037afa7e8
jojo <jojo@localhost.localdomain>
2016-09-17 23:43:18 +0530

    alpha UI dev

--- media_server/mnode/views.py
+++ media_server/mnode/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 import nmap
 
 from helpers import get_lan_ip
-
 
 def scan_list(request):
 
@@ -12,6 +12,11 @@ def scan_list(request):
     scanner = nmap.PortScanner()
     scanner.scan(hosts=hosts, arguments=nmap_args)
     hostList = []
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        client_ip = x_forwarded_for.split(',')[0]
+    else:
+        client_ip = request.META.get('REMOTE_ADDR')
 
     for ip in scanner.all_hosts():
         host = {"ip" : ip}
@@ -20,9 +25,8 @@ def scan_list(request):
         if "mac" in scanner[ip]["addresses"]:
           host["mac"] = scanner[ip]["addresses"]["mac"].upper()
         hostList.append(host)
-
     return render(
         request,
-        'templates/device_list.html',
-        {'hostList': hostList}
+        'device_list.html',
+        {'hostList': hostList, 'clientIp' : client_ip}
     )
