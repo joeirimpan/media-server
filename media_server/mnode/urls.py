972842d51e101489474bae07c2eae45b2c786b86
Joe Paul <joe.paul@fulfil.io>
2016-09-20 01:53:26 +0530

    Add file upload

--- media_server/mnode/urls.py
+++ media_server/mnode/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.scan_list, name="scan_list"), 
+    url(r'^$', views.scan_list, name='scan_list'),
+    url(r'^list/$', views.list, name='list')
 ]
--- media_server/mnode/views.py
+++ media_server/mnode/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+# -*- coding: utf-8 -*-
 import nmap
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from models import Document
+from forms import DocumentForm
 from helpers import get_lan_ip
 
-def scan_list(request):
 
+def scan_list(request):
     hosts= str(get_lan_ip()) + "/24"
-    nmap_args = "-sn"
+    nmap_args = "-sP"
     scanner = nmap.PortScanner()
     scanner.scan(hosts=hosts, arguments=nmap_args)
     hostList = []
@@ -19,14 +24,40 @@ def scan_list(request):
         client_ip = request.META.get('REMOTE_ADDR')
 
     for ip in scanner.all_hosts():
+        print 'Host : %s (%s)' % (ip, scanner[ip].hostname())
         host = {"ip" : ip}
         if "hostname" in scanner[ip]:
           host["hostname"] = scanner[ip]["hostname"]
         if "mac" in scanner[ip]["addresses"]:
           host["mac"] = scanner[ip]["addresses"]["mac"].upper()
         hostList.append(host)
+
     return render(
         request,
         'device_list.html',
         {'hostList': hostList, 'clientIp' : client_ip}
+    )
+
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render(
+        request,
+        'list.html',
+        {'documents': documents, 'form': form}
     )
